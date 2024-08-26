30d0b9753a10f0500ff7b37bed0306889e561d62
torbenstaud <105047712+torbenstaud@users.noreply.github.com>
2024-08-26 15:39:25 +0200

    frechet r fixed
    added confidence intervalls for shape estimation in the r fixed frechet estimation setting

--- scripts/mle/fixedR/bst/3generateVarEstPlots.R
+++ scripts/mle/fixedR/bst/3generateVarEstPlots.R
@@ -71,6 +71,108 @@ if(FALSE){
   )
 }
 
+#Confidence Intervals----
+load(here("scripts/mle/fixedR/bst/data/CiFreRfix"))
+ciFreTibRfix
+#create tibbles----
+
+#create factors----
+ciFreTibRFixPlot <- ciFreTibRfix %>% filter(
+  beta %in% c(0, 0.5)
+)
+
+
+
+
+ciFreTibRFixPlot$k <- factor(ciFreTibRFixPlot$k, levels = c(2,3,0,1),
+                             labels = c(parse(text = TeX("cb(2)")), 
+                                        parse(text = TeX("cb(3)")), 
+                                        parse(text = TeX("sb")),
+                                        parse(text = TeX("db"))
+                             )
+)
+ciFreTibRFixPlot$marginal <- factor(ciFreTibRFixPlot$marginal,
+                                    levels = c(3,4),
+                                    labels = c("Fréchet", "Pareto")
+)
+
+ciFreTibRFixPlot$beta <- factor(ciFreTibRFixPlot$beta, 
+                                levels = c(0, 0.25, 0.5, 0.75),
+                                labels = c(parse(text = TeX("i.i.d.")), 
+                                           parse(text = TeX("ARMAX $0.25$")), 
+                                           parse(text = TeX("ARMAX $0.5$")), 
+                                           parse(text = TeX("ARMAX $0.75$"))
+                                )
+)
+
+ciFreTibRFixPlot$alpha <- factor(ciFreTibRFixPlot$alpha, 
+                                 levels = c(0.5, 1, 1.5), 
+                                 labels = c(parse(text = TeX("$\\alpha_0 = 0.5$")),
+                                            parse(text = TeX("$\\alpha_0 = 1$")),
+                                            parse(text = TeX("$\\alpha_0 = 1.5$")))
+)
+##plots----
+ciCovPlot <- 
+  ciFreTibRFixPlot %>%   filter( m != 31) %>% 
+  filter(k != "sb") %>% 
+  ggplot()+
+  geom_line(aes(x = m, y = empCov, col = k))+
+  geom_hline(yintercept = 0.95, col = "black", linetype = "dashed")+
+  facet_grid(alpha~beta+marginal, scales = "free_y", labeller = label_parsed)+
+  labsPlot+
+  labs( y = paste0("Average empirical coverage"),
+        title = "Confidence intervals for the shape")+
+  themePlot+
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.title.x = element_blank())
+ciCovPlot
+
+#rescale <- 10
+ciWidthPlot <-
+  ciFreTibRFixPlot %>%  
+  filter(k != "sb") %>% 
+  ggplot()+
+  geom_line(aes(x = m, y = avgWidth, col = k))+
+  facet_grid(alpha~beta+marginal, scales = "free_y", labeller = label_parsed)+
+  labsPlot+
+  labs( y = paste0("Width"),
+        title = "Average width of confidence intervals")+
+  themePlot
+ciWidthPlot
+
+##now relative width Plot
+ciWidthDb <- ciFreTibRFixPlot %>% filter(k == "db") %>% select(-c(k,empCov)) %>% 
+  rename(dbWidth = "avgWidth")
+ciWidthRel <- left_join(ciFreTibRFixPlot, ciWidthDb) %>% 
+  filter(k %in% c("cb(2)", "cb(3)")) %>% mutate(relWidth = dbWidth/avgWidth)
+ciWidthRelPlot <- ciWidthRel %>% filter( m != 31) %>% 
+  ggplot()+
+  geom_line(aes(x = m, y = relWidth, col = k))+
+  geom_hline(yintercept = 1, col = "black", linetype = "dashed")+
+  facet_grid(alpha~beta+marginal, scales = "free_y", labeller = label_parsed)+
+  labsPlot+
+  labs( y = paste0("Relative average width"),
+        title = "Average relative width of confidence intervals",
+  )+
+  scale_y_continuous(limits = c(0.95, 1.25), breaks = c(1,1.1,1.2))+
+  themePlot+
+  theme(strip.text.x = element_blank(), plot.title = element_blank())
+
+ciWidthRelPlot
+
+combCiPlot <- 
+  ggarrange(ciCovPlot, ciWidthRelPlot, nrow = 2, align = "v", common.legend = T, 
+            legend = "bottom")
+combCiPlot
+
+if(FALSE){
+  ggsave(combCiPlot, path = here("results/"), 
+         filename = "plotFreFixRCi.pdf", device = "pdf",
+         width = 10, height = 10, 
+  )
+}
+
 
 
 
